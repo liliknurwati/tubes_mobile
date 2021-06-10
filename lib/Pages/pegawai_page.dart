ec3926453989add30e2e7d3f87bac30e9b6fb917
= <=>
2021-06-11 06:31:33 +0700

    membuat halaman untuk pegawai

---
+++ lib/Pages/pegawai_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PegawaiPage extends StatefulWidget{
+  @override
+  PegawaiPageState createState() => PegawaiPageState();
+
+}
+
+class PegawaiPageState extends State<PegawaiPage>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
